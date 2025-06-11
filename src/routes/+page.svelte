affa51f524ac699c0e8a4bbb5126fe1ae3fd82b0
lya <lasvmateo@gmail.com>
2025-06-11 21:20:44 +0200

    feat: update Tailwind CSS typography version and refactor Navbar for improved dark mode toggle functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script>
 	import Navbar from './Navbar/+page.svelte';
     import Grid from './Grid/+page.svelte';
-    import Item from './Item/+page.svelte'; 
 </script>
-<div class=" bg-black min-h-screen text-white">  
+<div >  
 <Navbar />
 <Grid />
 </div>
--- src/routes/Navbar/+page.svelte
+++ src/routes/Navbar/+page.svelte
@@ -15,19 +15,17 @@
 </script>
 
 <div class="sticky flex flex-row place-content-around border-2 rounded-b-3xl bg-gradient-to-b from-neutral-950 to-neutral-800 text-white">
-  <div class="border-1 h-15 sticky rounded-3xl basis-1/8 text-center">
-    <a href="/"><img src="src\img\Icon\home_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Home" class="h-8 w-8 inline-block" /></a>
+  <div class="border-1 h-15 sticky rounded-3xl basis-1/8 text-center flex items-center justify-center">
+    <button on:click={() => window.location.href = '/'}><img src="src\img\Icon\toggle_off_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg" alt="Home" class="h-8 w-8 inline-block" /></button>
   </div>
-  <div class="border-1 h-15 sticky rounded-3xl basis-1/4 text-center">
-    <a href="/cart"><img src="src\img\Icon\shopping_cart_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Cart" class="h-8 w-8 inline-block" /></a>
+  <div class="border-1 h-15 sticky rounded-3xl basis-1/4 text-center flex items-center justify-center">
+    <button on:click={() => window.location.href = '/cart'}><img src="src\img\Icon\shopping_cart_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg" alt="Cart" class="h-8 w-8 inline-block" /></button>
     </div>
   <div class="border-1 h-15 bticky rounded-3xl basis-1/3 bg-[url(img\silusins.png)] bg-contain bg-no-repeat bg-origin-border bg-center"></div>
-  <div class="border-1 h-15 sticky  rounded-3xl basis-1/4 text-center">
-    <a href="/account"><img src="src\img\Icon\account_circle_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Account profile" class="h-8 w-8 inline-block" /></a>
+  <div class="border-1 h-15 sticky  rounded-3xl basis-1/4 text-center flex items-center justify-center">
+    <button on:click={() => window.location.href = '/account'}><img src="src\img\Icon\account_circle_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg" alt="Account profile" class="h-8 w-8 inline-block" /></button>
   </div>
-  <div class="border-1 h-15 sticky  rounded-3xl basis-1/8 text-center">
+  <div class="border-1 h-15 sticky  rounded-3xl basis-1/8 text-center flex items-center justify-center">
     <button on:click={toggleDarkMode}><img src={icon} alt="Activate light mode" class="h-8 w-8 inline-block" /></button>
   </div>
 </div>
-
-
