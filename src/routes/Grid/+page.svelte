a319baa489b1bfa362987cd1de0f23782e3b33b0
lya <lasvmateo@gmail.com>
2025-06-11 14:24:48 +0200

    feat: add new icons and images, implement dark mode toggle in Navbar
    - Added new SVG icons for account circle, dark mode, light mode, and shopping cart in both black and white variants.
    - Introduced new images in the Visuel directory.
    - Removed unused PNG images to clean up the asset directory.
    - Updated the Grid page layout for better alignment.
    - Enhanced the Navbar with a dark mode toggle feature, allowing users to switch between light and dark themes.

--- src/routes/Grid/+page.svelte
+++ src/routes/Grid/+page.svelte
@@ -4,7 +4,7 @@
   const productIds = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 </script>
 
-<div class="grid grid-cols-3 gap-6 p-4">
+<div class="grid grid-cols-3 gap-6 p-4 place-content-around place-items-center">
   {#each productIds as id}
     <Item productId={id} />
   {/each}
--- src/routes/Navbar/+page.svelte
+++ src/routes/Navbar/+page.svelte
@@ -1,6 +1,33 @@
-<div class="sticky flex flex-row place-content-between border-2 rounded-b-3xl border-amber-300 bg-amber-200 ">
+<script>
+  let darkMode = false;
+  let icon = "src/img/Icon/light_mode_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg";
 
-  <div class="border-1 max-h-20 ticky rounded-3xl basis-1/4 text-center">Contact</div>
-  <div class="border-1 min-h-20 bticky rounded-3xl basis-1/3 bg-[url(img\silusins.png)] bg-contain bg-no-repeat bg-origin-border bg-center"></div>
-  <div class="border-1 max-h-20 sticky  rounded-3xl basis-1/4 text-center">Comte</div>    
+  function toggleDarkMode() {
+    darkMode = !darkMode;
+    if (darkMode) {
+      icon = "src/img/Icon/dark_mode_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg";
+      document.documentElement.classList.add('dark');
+    } else {
+      icon = "src/img/Icon/light_mode_40dp_FFFFFF_FILL0_wght400_GRAD0_opsz40.svg";
+      document.documentElement.classList.remove('dark');
+    }
+  }
+</script>
+
+<div class="sticky flex flex-row place-content-around border-2 rounded-b-3xl bg-gradient-to-b from-neutral-950 to-neutral-800 text-white">
+  <div class="border-1 h-15 sticky rounded-3xl basis-1/8 text-center">
+    <a href="/"><img src="src\img\Icon\home_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Home" class="h-8 w-8 inline-block" /></a>
+  </div>
+  <div class="border-1 h-15 sticky rounded-3xl basis-1/4 text-center">
+    <a href="/cart"><img src="src\img\Icon\shopping_cart_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Cart" class="h-8 w-8 inline-block" /></a>
+    </div>
+  <div class="border-1 h-15 bticky rounded-3xl basis-1/3 bg-[url(img\silusins.png)] bg-contain bg-no-repeat bg-origin-border bg-center"></div>
+  <div class="border-1 h-15 sticky  rounded-3xl basis-1/4 text-center">
+    <a href="/account"><img src="src\img\Icon\account_circle_40dp_000000_FILL0_wght400_GRAD0_opsz40.svg" alt="Account profile" class="h-8 w-8 inline-block" /></a>
+  </div>
+  <div class="border-1 h-15 sticky  rounded-3xl basis-1/8 text-center">
+    <button on:click={toggleDarkMode}><img src={icon} alt="Activate light mode" class="h-8 w-8 inline-block" /></button>
+  </div>
 </div>
+
+
