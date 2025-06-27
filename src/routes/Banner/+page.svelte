2421508f026051f48297be934e6c60d0fdf0b090
lya <lasvmateo@gmail.com>
2025-06-27 21:15:22 +0200

    feat: Add Banner component and integrate it into the main page layout

---
+++ src/routes/Banner/+page.svelte
@@ -0,0 +1,7 @@
+<div class=" relative w-[90vw] max-w-[1200px] mx-auto">
+    <div class="flex flex-col items-center justify-center gap-4 p-4">
+        <h1 class="text-2xl font-bold">Bienvenue sur Alisee</h1>
+        <p class="text-lg">Votre boutique en ligne pour les produits artisanaux</p>
+    </div>
+
+</div>
