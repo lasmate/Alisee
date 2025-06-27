f6ef2581c16de9a67d84d2725f01d83e5c7da2ea
lya <lasvmateo@gmail.com>
2025-06-27 22:17:03 +0200

    feat: implement user registration API endpoint and enhance registration form with additional typesafe fields

---
+++ src/routes/api/account/register/+server.ts
@@ -0,0 +1,27 @@
+/*
+*API endpoint for user registration
+*Handles user registration and returns user data if successful
+*/
+import type { RequestHandler } from './$types';
+import { db } from '$lib/server/db';
+import { users } from '$lib/server/db/schema/Users';
+import { eq } from 'drizzle-orm';
+import { json, error } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const {name,surname,email,password} = await request.json();
+
+    if (!name || !surname || !email || !password) {
+        return error(400, 'Name, surname, email, and password are required');
+    }
+    // Check if user already exists
+    const exitingUser = await db.select().from(users).where(eq(users.email,email)).limit(1);
+    if (exitingUser.length > 0) {
+        return error(409, 'User already exists with this email');
+    }
+
+    // Create new user
+    const newUser = await db.insert(users).values({ name, surname, email, password }).returning();
+
+    return json({ user: newUser });
+};
